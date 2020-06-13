24216e2e433e8574fa352147ffa7fdf18ccdb8e5
Ibrahimm18 <65872954+Ibrahimm18@users.noreply.github.com>
2020-06-13 19:42:40 +0530

    Add files via upload

---
+++ src/Robots.js
@@ -0,0 +1,63 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Mohammed Ibrahim',
+    username: 'MiB',
+    email: 'Mib@april.biz'
+  },
+  {
+    id: 2,
+    name: 'Raizul Luqman',
+    username: 'Raiz',
+    email: 'RaizulLuq@june.biz'
+  },
+  {
+    id: 3,
+    name: 'Ayysha Meharaj',
+    username: 'Aish',
+    email: 'Aish@november.biz'
+  },
+  {
+    id: 4,
+    name: 'Mohammed Ubaidh',
+    username: 'MSU',
+    email: 'Ubaidh@december.biz'
+  },
+  {
+    id: 5,
+    name: 'Mohammed Riffath',
+    username: 'MRF',
+    email: 'Riff@jasper.biz'
+  },
+  {
+    id: 6,
+    name: 'Arshu Afrose',
+    username: 'Arsh',
+    email: 'Arsh@october.biz'
+  },
+  {
+    id: 7,
+    name: 'Mohammed Affan',
+    username: 'Affan',
+    email: 'Affan@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Mohammed Arfan',
+    username: 'Arfan',
+    email: 'Arfan@rosamond.biz'
+  },
+  {
+    id: 9,
+    name: 'Zufar Abbas',
+    username: 'Zufar',
+    email: 'Zufar@june.biz'
+  },
+  {
+    id: 10,
+    name: 'Clementina DuBuque',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz'
+  }
+  
+];
---
+++ src/components/CardList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Card from './Card';
+const CardList = ({robots}) => { 
+	return (
+		<div>
+	   {
+	   	robots.map( (count,i) => {
+		return <Card 
+		key={i} 
+		id={robots[i].id} 
+		username={robots[i].username} 
+		name={robots[i].name} 
+		email={robots[i].email}
+		/>
+	})
+	   }
+	
+	</div>  
+		);
+}
+export default CardList;
